27f094b684236faf7d272d532fdbb96631ea5fc8
Jamova01 <jorgemova01@gmail.com>
2025-06-01 16:40:16 -0500

    feat(header): create HeaderComponent with side menu toggle logic

---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
+}
