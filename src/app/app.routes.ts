0d1c4a6768d91c573b815f474aa80ff5a7de7f93
Jamova01 <jorgemova01@gmail.com>
2025-06-02 15:40:18 -0500

    feat: add custom 404 Not Found page with navigation link to homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from '@products/pages/list/list.component';
+import { NotFoundComponent } from './domains/general/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
