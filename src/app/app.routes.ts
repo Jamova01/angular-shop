bb67a670299613d47b381a3d9ad7f3fcb9a5a544
Jamova01 <jorgemova01@gmail.com>
2025-06-02 16:02:09 -0500

    feat: nest ListComponent inside LayoutComponent via child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { ListComponent } from '@products/pages/list/list.component';
 import { NotFoundComponent } from './domains/general/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ListComponent,
+      },
+    ],
   },
   {
     path: '**',
