528ed21b22f998830409df232ef845e12f7794e2
Jamova01 <jorgemova01@gmail.com>
2025-06-02 12:04:39 -0500

    Apply uppercase pipe to product title in product component

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
