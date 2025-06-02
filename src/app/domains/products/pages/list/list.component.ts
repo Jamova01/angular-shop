d719661084bb4160c9cd0aac3dc52a1b29e42ff6
Jamova01 <jorgemova01@gmail.com>
2025-06-02 16:03:09 -0500

    feat: implement ListComponent with CommonModule and services

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { ProductComponent } from '@products/components/product/product.component';
 
 import { Product } from '@shared/models/product.model';
@@ -12,7 +11,7 @@ import { ProductService } from '@shared/services/product.service';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
